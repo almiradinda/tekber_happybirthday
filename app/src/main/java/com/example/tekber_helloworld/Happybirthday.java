49bd64497868b46d50821ce9de045ef40ad12052
andry027 <andryaprian27@gmail.com>
2019-09-20 08:31:23 +0700

    update tugas layout happy birthday

---
+++ app/src/main/java/com/example/tekber_helloworld/Happybirthday.java
@@ -0,0 +1,14 @@
+package com.example.tekber_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Happybirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happybirthday);
+    }
+}
